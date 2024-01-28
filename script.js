573e035d03e8929d3139fff33821214aa4814edd
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-01-28 11:30:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -108,14 +108,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Developer", "Freelancer", "Student BCA'26"],
+    strings: ["Developer", "Freelancer", "Student MCA'26"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Developer", "Freelancer", "Student BCA'26"],
+    strings: ["Developer", "Freelancer", "Student MCA'26"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
